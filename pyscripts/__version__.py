d2d4e77c334db791f6243a6c068be8bed8b60671
Jerome Guterl <guterlj@fusion.gat.com>
2020-05-04 23:58:20 -0700

    timing

--- pyscripts/__version__.py
+++ pyscripts/__version__.py
@@ -1,4 +1,4 @@
 __version__='v7.1.0.0'
 GitTag='v7.1.0.0'
 GitBranch='paralleljac'
-GitHash='c59577711f6e6236d89a7e0aea303922e6454122'
+GitHash='4b27068a40dfce167ea5c40ca2a24e48f3241461'
--- src/bbb/paralleljac.f90
+++ src/bbb/paralleljac.f90
@@ -159,7 +159,7 @@ subroutine OMPCollectJacobian(neq,nnzmx,rcsc,icsc,jcsc,nnzcumout)
     if (OMPTimingJacRow.gt.0) then
     open (newunit = iunit, file = 'omptiming.dat')
     do ith=1,neq
-    write(iunit,*)iv,OMPTimeJacRow(iv)
+    write(iunit,*) iv,OMPTimeJacRow(iv)
     enddo
     close(iunit)
     endif
