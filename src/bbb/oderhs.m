455f5676f4b63b62759555b67f24c73ff0e18da8
Jerome Guterl <guterlj@fusion.gat.com>
2020-06-08 19:53:49 -0700

    timingpandf added

--- src/bbb/oderhs.m
+++ src/bbb/oderhs.m
@@ -4864,12 +4864,12 @@ c...  Finally, reset some source terms if this is a Jacobian evaluation
          if (xc .ge. 0 .and. yc .ge. 0) then
             ix1 = ixm1(xc,yc)
             if(isimpon.gt.0) pwrzec(xc,yc) = pradold
-*%
-*%            if (fixpwrebkg.gt.0) then
-*%            pwrebkg(xc,yc)=pwrbkg_c
-*%            else
+
+            if (fixpwrebkg.gt.0) then
+            pwrebkg(xc,yc)=pwrbkg_c
+            else
             pwrebkg(xc,yc) = pwrebkgold
-*%            endif
+            endif
 
             pwribkg(xc,yc) = pwribkgold
             erliz(xc,yc) = erlizold
